8ce204d7c4e8d85bf1eeffc504399d3338282883
Tim Harris <raymondtimothyharris@gmail.com>
2016-05-21 21:48:29 -0700

    Working MergeSort
    Have implemented a working version of merge sort. Just need to add some
    optimization to it.

--- SwiftAlgoriths/SwiftAlgoriths/MergeSort.swift
+++ SwiftAlgoriths/SwiftAlgoriths/MergeSort.swift
@@ -9,48 +9,46 @@
 import Foundation
 
 extension Array where Element :  Comparable {
-    mutating func MergeSort(index: Int, size: Int) -> Void {
-        var mid: Int
-        if index < size {
-            mid = (size + index)/2
-            self.MergeSort(index, size: mid)
-            self.MergeSort(mid + 1, size: size)
-            self.Merge(index, last: mid, current: size)
-        }
+    mutating func MergeSort() -> Array<Element> {
+        guard self.count > 1 else { return self }
+        let mid = self.count / 2
+        
+        var leftArray = Array<Element>(self[0..<mid])
+        let left = leftArray.MergeSort()
+        var rightArray = Array<Element>(self[mid..<self.count])
+        let right = rightArray.MergeSort()
+        return self.Merge(left, right: right)
     }
     
-    mutating func Merge(first: Int, last: Int, current: Int) {
+    mutating func Merge(left: Array<Element>, right: Array<Element>) ->Array<Element> {
+        var leftIndex = 0
+        var rightIndex = 0
+        var sorted = Array<Element>()
         
-        let left = last - first + 1
-        let right = current - last
-        
-        var leftArray = Array()
-        var rightArray = Array()
-        
-        for i in 0..<left {
-            leftArray.append(self[first + i - 1])
-        }
-        for j in 0..<right {
-            rightArray.append(self[last + j])
-        }
-        
-        // LeftArr insert Max
-        // RightArr insert Max
-        
-        leftArray.append(INTMAX_MAX as! Element)
-        rightArray.append(INTMAX_MAX as! Element)
-        
-        var i = 0
-        var j = 0
-        
-        for k in (first - 1)..<current {
-            if leftArray[i] <= rightArray[j] {
-                self[k] = leftArray[i]
-                i = i + 1
+        while leftIndex < left.count && rightIndex < right.count {
+            if left[leftIndex] < right[rightIndex] {
+                sorted.append(left[leftIndex])
+                leftIndex += 1
+            } else if left[leftIndex] > right[rightIndex] {
+                sorted.append(right[rightIndex])
+                rightIndex += 1
             } else {
-                self[k] = rightArray[j]
-                j = j + 1
+                sorted.append(left[leftIndex])
+                leftIndex += 1
+                sorted.append(right[rightIndex])
+                rightIndex += 1
             }
         }
+        
+        while leftIndex < left.count {
+            sorted.append(left[leftIndex])
+            leftIndex += 1
+        }
+        while  rightIndex < right.count {
+            sorted.append(right[rightIndex])
+            rightIndex += 1
+        }
+        
+        return sorted
     }
 }
